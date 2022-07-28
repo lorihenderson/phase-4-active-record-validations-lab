418b308611a06e6b3776f4134b922a79af71ace3
Lori Henderson <lorim95@gmail.com>
2022-07-28 10:23:28 -0400

    commit tree 06fbc4b0c6a0a1437df1cc9494282cd360552502

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    validates :name, :phone_number, presence: true
+    validates :name, uniqueness: true
+    validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, :content, :summary, :category, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait
+    
+    # custom validation
+    def is_clickbait
+        if title.present? && !title.include?("Won't Believe") && !title.include?("Secret") && !title.include?("Top") && !title.include?("Guess")
+
+            errors.add(:title, "Title isn't clickbaity enough.")
+        end
+    end
+
+
 end
